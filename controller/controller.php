f28c0d4197449fc9901ebeafafe49a2a09a1a811
David King <imkingdavid@gmail.com>
2012-10-25 14:59:28 -0400

    [initial] Fix doc block spacing and headings

--- controller/controller.php
+++ controller/controller.php
@@ -1,15 +1,15 @@
 <?php
 /**
-*
-* @package pun
-* @copyright (c) 2012 David King (imkingdavid)
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-*
-*/
+ *
+ * @package pun
+ * @copyright (c) 2012 David King (imkingdavid)
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
 
 /**
-* @ignore
-*/
+ * @ignore
+ */
 if (!defined('IN_PHPBB'))
 {
 	exit;
